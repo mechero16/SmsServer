92af5a13d4faed6fea08dcaf5eb0b7793cda520d
mechero16 <156899689+mechero16@users.noreply.github.com>
2024-09-02 15:47:17 +0530

    Update Sms.js

--- Sms.js
+++ Sms.js
@@ -7,7 +7,8 @@ const app = express();
 const port = 3001;
 
 // Twilio credentials
-
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 // Middleware
