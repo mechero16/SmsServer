968dbd39be22802a4d181fd7436a9af5679a6008
mechero16 <156899689+mechero16@users.noreply.github.com>
2024-09-02 15:42:17 +0530

    Add files via upload

---
+++ Sms.js
@@ -0,0 +1,50 @@
+import express from "express";
+import twilio from "twilio";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+const port = 3001;
+
+// Twilio credentials
+
+const client = twilio(accountSid, authToken);
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/sendsms", async (req, res) => {
+  const contacts = req.body.contacts;
+
+  if (!contacts) {
+    return res.status(400).json({ error: "No contacts provided" });
+  }
+
+  const results = [];
+  const errors = [];
+  const date = new Date();
+  const formattedDate = date.toDateString();
+
+  for (const [name, number] of Object.entries(contacts)) {
+    try {
+      const message = await client.messages.create({
+        body: `Dear Parent/Guardian,\n
+We would like to inform you that ${name} is absent from college today, ${formattedDate}. Please ensure that [he/she] catch up on missed work and assignments. If you have any questions or need assistance, feel free to contact us.\n
+Thank you for your attention.`,
+        from: "+13344328465",
+        to: `+91${number}`,
+      });
+
+      results.push({ name, number, sid: message.sid });
+    } catch (error) {
+      errors.push({ name, number, error: error.message });
+    }
+  }
+
+  res.status(200).json({ results, errors });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
